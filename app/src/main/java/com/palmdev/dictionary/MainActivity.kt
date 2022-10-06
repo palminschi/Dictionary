3511f03ccca1c8618fdbccda0755d65784a836be
palminschi <mihaipalminschi@gmail.com>
2022-10-06 12:24:43 +0200

    Dependencies updated

--- app/src/main/java/com/palmdev/dictionary/MainActivity.kt
+++ app/src/main/java/com/palmdev/dictionary/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.palmdev.dictionary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.palmdev.dictionary.ui.theme.DictionaryTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            DictionaryTheme {
+
+            }
+        }
     }
 }
